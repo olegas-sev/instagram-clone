a0fd52b5f8d7bb772f8cdf1f6b121ddc404dd375
Olegas Sevcenko <sevcenko.olegas@gmail.com>
2021-10-14 14:56:17 +0200

    Next js init, with two parts of navbar done

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+
+      {/* Header */}
+      <Header />
+
+
+      {/* Feed */}
+
+      {/* Footer */}
+
+    </div>
+  )
+}
